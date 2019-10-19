af3c7421b0225a0d93656f5f13e943f2a110eb54
Jana Pušová <jana@pushka.cz>
2019-10-19 14:47:29 +0200

    Editing password check, changing indentation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 author = Jana Pušová
 '''
 texts = [
-  '''Situated about 10 miles west of Kemmerer, 
+    '''Situated about 10 miles west of Kemmerer, 
 Fossil Butte is a ruggedly impressive 
 topographic feature that rises sharply 
 some 1000 feet above Twin Creek Valley 
@@ -32,11 +32,11 @@ garpike and stingray are also present.'''
 ]
 
 users = {
-  'bob' : '123',
-  'ann' : 'pass123',
-  'mike' : 'password123',
-  'lizz' : 'pass123'
-  }
+    'bob' : '123',
+    'ann' : 'pass123',
+    'mike' : 'password123',
+    'lizz' : 'pass123'
+    }
 
 # Greeting user
 print()
@@ -49,117 +49,117 @@ print()
 print('YOUR CREDENTIALS:')
 username = input('Username: ')
 
-if username not in list(users.keys()):
-  print("Username doesn't exist, try again")
-else: 
-  password = input('Password: ')
-  if users[username] == password:
-    print('-' * 60)
-    print('ACCESS GRANTED')
-    print('-' * 60)
-    print()
-    print('OUR TEXTS: ')
-    print('1: ' + str(texts[0]))
-    print()
-    print('2: ' + str(texts[1]))
-    print()
-    print('3: ' + str(texts[2]))
-    print()
-  else:
-    print('ACCESS DENIED')
-    exit()
+if username not in users:
+    print("Username doesn't exist, try again")
+else:
+    password = input('Password: ')
+    if users[username] == password:
+        print('-' * 60)
+        print('ACCESS GRANTED')
+        print('-' * 60)
+        print()
+        print('OUR TEXTS: ')
+        print('1: ' + str(texts[0]))
+        print()
+        print('2: ' + str(texts[1]))
+        print()
+        print('3: ' + str(texts[2]))
+        print()
+    else:
+        print('ACCESS DENIED')
+        exit()
 
 # Getting number of text to be analyzed
-  choice = int(input('Please select the requested text (1, 2, 3): '))
-  print()
-  if choice not in [1, 2, 3]:
+choice = int(input('Please select the requested text (1, 2, 3): '))
+print()
+if choice not in [1, 2, 3]:
     print('Wrong number, try again')
-  else:
+else:
     print('-' * 60)
     print('STATISTICS FOR THE TEXT NUMBER ' + str(choice) + ':')
     print('-' * 60)
 
 # Statistics: number of words in total
-    preselection = texts[choice-1].split(' ')
-    selection = [x.replace(',','').replace('.','').strip() for x in preselection]
-    print('Number of words in total: ' + str(len(selection)))
+preselection = texts[choice-1].split(' ')
+selection = [x.replace(',','').replace('.','').strip() for x in preselection]
+print('Number of words in total: ' + str(len(selection)))
 
 # Statistics: number of words starting with capital
-    titled = 0
-    i = 0
+titled = 0
+i = 0
 
-    while i < len(selection):
-      if selection[i].istitle():
+while i < len(selection):
+    if selection[i].istitle():
         titled += 1
         i += 1
-      else:
+    else:
         i += 1
         continue
-    print('Number of words starting with capital letter: ' + str(titled))
+print('Number of words starting with capital letter: ' + str(titled))
 
 # Statistics: number of uppercase words
-    uppercase = 0
-    i = 0
+uppercase = 0
+i = 0
 
-    while i < len(selection):
-      if selection[i].isupper() and selection[i].isalpha():
+while i < len(selection):
+    if selection[i].isupper() and selection[i].isalpha():
         uppercase += 1
         i += 1
-      else:
+    else:
         i += 1
         continue
-    print('Number of uppercase words: ' + str(uppercase))
+print('Number of uppercase words: ' + str(uppercase))
 
 # Statistics: number of lowercase words
-    lowercase = 0
-    i = 0
+lowercase = 0
+i = 0
 
-    while i < len(selection):
-      if selection[i].islower(): # pure lowercase = if selection[i].islower() and selection[i].isalpha():
+while i < len(selection):
+    if selection[i].islower():
         lowercase += 1
         i += 1
-      else:
+    else:
         i += 1
         continue
-    print('Number of lowercase words: ' + str(lowercase))
+print('Number of lowercase words: ' + str(lowercase))
 
 # Statistics: number of numeric-only words
-    numeric = 0
-    numbers = []
-    i = 0
+numeric = 0
+numbers = []
+i = 0
 
-    while i < len(selection):
-      if selection[i].isnumeric():
+while i < len(selection):
+    if selection[i].isnumeric():
         numeric += 1
         numbers.append(float(selection[i])) 
         i += 1
-      else:
+    else:
         i += 1
         continue
-    print('Number of numeric-only words: ' + str(numeric))
-    print('-' * 60)
+print('Number of numeric-only words: ' + str(numeric))
+print('-' * 60)
 
 # Statistics: bar charts - frequencies of word lengths in the text
-    lenght = [len(x) for x in selection]
-    unique = list(set(lenght))
+lenght = [len(x) for x in selection]
+unique = list(set(lenght))
     
-    i = 0
+i = 0
     
-    while unique:
-      if i < len(unique):
+while unique:
+    if i < len(unique):
         lenght.count(unique[i])
         print(str(unique[i]) + ' ' + str(lenght.count(unique[i]) * '*') + ' ' + str(lenght.count(unique[i])))
         i += 1
-      else:
+    else:
         break
                 
-    print('-' * 60)
+print('-' * 60)
 
 # Statistics: sum of numeric words
-    print('Sum of all numeric words: ' + str(sum(numbers)))
-    print('-' * 60)
-    print()
+print('Sum of all numeric words: ' + str(sum(numbers)))
+print('-' * 60)
+print()
 
 # End of statistics
-    print('THANK YOU FOR USING OUR TEXT ANALYZER!')
-    print()
+print('THANK YOU FOR USING OUR TEXT ANALYZER!')
+print()
